98d81860fad80af7bfb267c6aa5601bab80ce050
manishMandal02 <manishmandalj@gmail.com>
2022-01-23 12:56:49 +0530

    upted about me sec

--- components/aboutMe/Index.tsx
+++ components/aboutMe/Index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import About from './About.tsx';
+import About from './About';
 import TechSystem from './TechSystem';
 
 const AboutMe = () => {
   return (
-    <div className=''>
+    <div className='  overflow-x-hidden'>
       {/* <div
         className=' h-40 mt-20 w-full'
         style={{
@@ -22,10 +22,15 @@ const AboutMe = () => {
           backgroundRepeat: 'repeat',
         }}
       ></div>
-      <div className='flex -mt-16 py-10 px-16'>
+      <div className='flex justify-between -mt-16 py-10 h-96 px-16'>
         <About />
         <TechSystem />
       </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore dicta magnam labore assumenda nemo
+        consequuntur asperiores ducimus adipisci facere odio, repudiandae ipsum dignissimos delectus in ea
+        debitis enim quo a!
+      </p>
     </div>
   );
 };
--- components/aboutMe/TechSystem.tsx
+++ components/aboutMe/TechSystem.tsx
@@ -1,32 +1,73 @@
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 import React from 'react';
 
 const TechSystem = () => {
-  const ballVariant = {
-    initial: {
-      x: 60,
-      y: 0,
-    },
+  const techStackContainerVariant = {
     animate: {
-      x: [60, -60],
-      y: [-100, 100],
+      rotate: 360,
       transition: {
         repeat: Infinity,
-        duration: 1.6,
-        type: 'tween',
+        duration: 20,
+        ease: 'linear',
       },
     },
   };
 
+  const techStackVariant = {
+    animate0: {
+      // rotate: -360,
+    },
+  };
+
+  const jsLogo: string =
+    'https://res.cloudinary.com/vastia/image/upload/v1615998801/portfolio/TechStack/javascript_ketvye.png';
+
+  const mernTech: string[] = [
+    'https://res.cloudinary.com/vastia/image/upload/v1615998803/portfolio/TechStack/react_wpgmkr.png',
+    'https://res.cloudinary.com/vastia/image/upload/v1642861709/portfolio/TechStack/nextjs_noqhb8.svg',
+    'https://res.cloudinary.com/vastia/image/upload/v1615998802/portfolio/TechStack/node-js_b2crs4.png',
+    'https://res.cloudinary.com/vastia/image/upload/v1642920970/portfolio/TechStack/express-1_pbocwy.png',
+  ];
+
   return (
-    <div className='py-2 px-2 w-1/2 flex items-center justify-center'>
-      <motion.div className='bg-red-400 border-2 border-slate-500 w-12 h-12 rounded-full'></motion.div>
+    <div className='py-4 px-4 w-1/2 flex items-center justify-center'>
       <motion.div
-        className='bg-emerald-300 border-2 border-slate-500 w-12 h-12 rounded-full'
-        variants={ballVariant}
+        variants={techStackContainerVariant}
         initial='initial'
         animate='animate'
-      ></motion.div>
+        className='bg-white flex items-center justify-center shadowPlane1  border-slate-200 w-60 h-60 rounded-full relative '
+      >
+        {/* tech stacks */}
+        <motion.div
+          className={`bg-white flex items-center justify-center shadowPlane0 shadow-gray-200  w-14 h-14 rounded-full absolute -top-6 left-24 py-2 px-2`}
+        >
+          <Image src={mernTech[0]} height={50} width={50} className='z-20' />
+        </motion.div>
+        <motion.div
+          className={`bg-white flex items-center justify-center shadowPlane0 w-14 h-14 rounded-full absolute top-24 -right-6 py-2 px-2`}
+        >
+          <Image src={mernTech[1]} height={50} width={50} className='z-20' />
+        </motion.div>
+        <motion.div
+          className={`bg-white flex items-center justify-center shadowPlane0 w-14 h-14 rounded-full absolute top-20 -left-6 py-2 px-2`}
+        >
+          <Image src={mernTech[2]} height={50} width={50} className='z-20' />
+        </motion.div>
+        <motion.div
+          className={`bg-white flex items-center justify-center shadowPlane0 w-14 h-14 rounded-full absolute top-52  left-20 py-2 px-2`}
+        >
+          <Image src={mernTech[3]} height={50} width={50} className='z-20 ' />
+        </motion.div>
+      </motion.div>
+      {/* JS in center */}
+      <motion.div
+        variants={techStackVariant}
+        animate='animate0'
+        className='bg-white flex items-center justify-center shadow-sm shadow-gray-300 w-14 h-14 rounded-full absolute'
+      >
+        <Image src={jsLogo} layout='fill' className='z-20 rounded-full ' />
+      </motion.div>
     </div>
   );
 };
